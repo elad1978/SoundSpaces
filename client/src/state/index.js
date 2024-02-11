f36b2abd60c1f6d1084d0ee458a27c56159a9e44
elad1978 <efrathalev@gmail.com>
2024-02-11 13:29:15 +0200

    setting client side

---
+++ client/src/state/index.js
@@ -0,0 +1,58 @@
+/**
+ * Creates a Redux slice for managing authentication state, with reducers and actions for
+ * setting the mode, logging in/out, getting friends, getting posts, and updating a post.
+ */
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    //actions are : params that we sent to the reducer,
+    //and the payload that we want to send back to the reducer
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends not exists.");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) {
+          return action.payload.post;
+        }
+        //technically, you can remove the else block
+        else {
+          return post;
+        }
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
